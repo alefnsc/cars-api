f489c6d159eaba05c0228a39af7c44f387b1af5f
Alexandre Augusto Fonseca <alexandralexandrefonsecach@gmail.com>
2024-02-27 18:39:37 -0300

    feat helmet cors prod deploy allowance

--- index.js
+++ index.js
@@ -8,6 +8,23 @@ app.use(express.json());
 
 app.use("/marcas", marcasRouter);
 
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'", "https://cars-api-8edp.onrender.com"],
+      fontSrc: ["'self'", "https://cars-api-8edp.onrender.com"],
+      scriptSrc: ["'self'", "https://cars-api-8edp.onrender.com"],
+      styleSrc: [
+        "'self'",
+        "'unsafe-inline'",
+        "https://cars-api-8edp.onrender.com",
+      ],
+      imgSrc: ["'self'", "data:", "https://cars-api-8edp.onrender.com"],
+      connectSrc: ["'self'", "https://cars-api-8edp.onrender.com"],
+    },
+  })
+);
+
 app.listen(3000, async () => {
   try {
     console.log("API Started!");
